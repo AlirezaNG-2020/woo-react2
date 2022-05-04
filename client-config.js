5b996256e76706c5dcf050ea89e2a8a92bb9f4de
woo-next <alirezanaghash2019@gmail.com>
2022-05-04 23:34:12 +0430

    four commit

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 const clientConfig = {
     siteUrl: 'http://localhost:3000',
-    graphqlUrl: 'http://127.0.0.1:8080/react/graphql',
+    graphqlUrl: 'localhost:8080/react/graphql',
 };
 
 export default clientConfig;
